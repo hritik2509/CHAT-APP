7adb250f0c67becf17922daf33d80c4f9e29b950
Ritik Nishad <hritiknishad1@gmail.com>
2024-04-18 16:12:05 +0530

    1st part completed - backend

---
+++ backend/models/user.model.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    fullName: {
+      type: String,
+      require: true,
+    },
+    userName: {
+      type: String,
+      require: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      require: true,
+      minlenght: 6,
+    },
+    gender: {
+      type: String,
+      require: true,
+      enum: ["male", "female"],
+    },
+    profilePic: {
+      type: String,
+      default: "",
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
