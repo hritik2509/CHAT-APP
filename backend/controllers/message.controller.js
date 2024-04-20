fd43ae4f1b9a668ae48d5dc70dd32d98fc8c4a75
Ritik Nishad <hritiknishad1@gmail.com>
2024-04-20 17:58:30 +0530

    socket completed

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -1,5 +1,6 @@
 import Conversation from "../models/conversation.model.js";
 import Message from "../models/message.model.js";
+import { getReceiverSocketId } from "../socket/socket.js";
 
 export const sendMessage = async (req, res) => {
   try {
@@ -27,12 +28,16 @@ export const sendMessage = async (req, res) => {
       conversation.messages.push(newMessage._id);
     }
 
-    // Socket IO functionality
-
     // await conversation.save();
     // await newMessage.save();
 
     await Promise.all([conversation.save(), newMessage.save()]);
+
+    // Socket IO functionality
+    const recieverSocketId = getReceiverSocketId(receiverId);
+    if (recieverSocketId) {
+      io.to(recieverSocketId).emit("newMessage", newMessage);
+    }
 
     res.status(201).json(newMessage);
   } catch (error) {
--- frontend/src/components/sidebar/Conversation.jsx
+++ frontend/src/components/sidebar/Conversation.jsx
@@ -1,10 +1,13 @@
-import React from "react";
+import { useSocketContext } from "../../context/SocketContext";
 import useConversation from "../../zustand/useConversation";
 
 const Conversation = ({ conversation, lastIdx, emoji }) => {
   const { selectedConversation, setSelectedConversation } = useConversation();
 
   const isSelected = selectedConversation?._id === conversation._id;
+  const { onlineUsers } = useSocketContext();
+  const isOnline = onlineUsers.includes(conversation._id);
+
   return (
     <>
       <div
@@ -13,7 +16,7 @@ const Conversation = ({ conversation, lastIdx, emoji }) => {
 			`}
         onClick={() => setSelectedConversation(conversation)}
       >
-        <div className="avatar online">
+        <div className={`avatar ${isOnline ? "online" : ""}`}>
           <div className="w-12 rounded-full">
             <img src={conversation.profilePic} alt="user avatar" />
           </div>
@@ -31,43 +34,81 @@ const Conversation = ({ conversation, lastIdx, emoji }) => {
     </>
   );
 };
-
 export default Conversation;
 
-// import { useSocketContext } from "../../context/SocketContext";
+// import React from "react";
 // import useConversation from "../../zustand/useConversation";
+// import { useSocketContext } from "../../context/SocketContext";
 
 // const Conversation = ({ conversation, lastIdx, emoji }) => {
-// 	const { selectedConversation, setSelectedConversation } = useConversation();
+//   const { selectedConversation, setSelectedConversation } = useConversation();
 
-// 	const isSelected = selectedConversation?._id === conversation._id;
-// 	const { onlineUsers } = useSocketContext();
-// 	const isOnline = onlineUsers.includes(conversation._id);
+//   const isSelected = selectedConversation?._id === conversation._id;
+//   const { onlineUser } = useSocketContext();
+//   const isOnline = onlineUser.includes(conversation._id);
+//   return (
+//     <>
+//       <div
+//         className={`flex gap-2 items-center hover:bg-sky-500 rounded p-2 py-1 cursor-pointer
+// 				${isSelected ? "bg-sky-500" : ""}
+// 			`}
+//         onClick={() => setSelectedConversation(conversation)}
+//       >
+//         <div className={`avatar ${isOnline ? "Online" : ""}`}>
+//           <div className="w-12 rounded-full">
+//             <img src={conversation.profilePic} alt="user avatar" />
+//           </div>
+//         </div>
 
-// 	return (
-// 		<>
-// <div
-// 	className={`flex gap-2 items-center hover:bg-sky-500 rounded p-2 py-1 cursor-pointer
-// 	${isSelected ? "bg-sky-500" : ""}
-// `}
-// 	onClick={() => setSelectedConversation(conversation)}
-// >
-// 				<div className={`avatar ${isOnline ? "online" : ""}`}>
-// 					<div className='w-12 rounded-full'>
-// 						<img src={conversation.profilePic} alt='user avatar' />
-// 					</div>
-// 				</div>
+//         <div className="flex flex-col flex-1">
+//           <div className="flex gap-3 justify-between">
+//             <p className="font-bold text-gray-200">{conversation.fullName}</p>
+//             <span className="text-xl">{emoji}</span>
+//           </div>
+//         </div>
+//       </div>
 
-// 				<div className='flex flex-col flex-1'>
-// 					<div className='flex gap-3 justify-between'>
-// 						<p className='font-bold text-gray-200'>{conversation.fullName}</p>
-// 						<span className='text-xl'>{emoji}</span>
-// 					</div>
-// 				</div>
-// 			</div>
-
-// 			{!lastIdx && <div className='divider my-0 py-0 h-1' />}
-// 		</>
-// 	);
+//       {!lastIdx && <div className="divider my-0 py-0 h-1" />}
+//     </>
+//   );
 // };
+
 // export default Conversation;
+
+// // import { useSocketContext } from "../../context/SocketContext";
+// // import useConversation from "../../zustand/useConversation";
+
+// // const Conversation = ({ conversation, lastIdx, emoji }) => {
+// // 	const { selectedConversation, setSelectedConversation } = useConversation();
+
+// // 	const isSelected = selectedConversation?._id === conversation._id;
+// // 	const { onlineUsers } = useSocketContext();
+// // 	const isOnline = onlineUsers.includes(conversation._id);
+
+// // 	return (
+// // 		<>
+// // <div
+// // 	className={`flex gap-2 items-center hover:bg-sky-500 rounded p-2 py-1 cursor-pointer
+// // 	${isSelected ? "bg-sky-500" : ""}
+// // `}
+// // 	onClick={() => setSelectedConversation(conversation)}
+// // >
+// // 				<div className={`avatar ${isOnline ? "online" : ""}`}>
+// // 					<div className='w-12 rounded-full'>
+// // 						<img src={conversation.profilePic} alt='user avatar' />
+// // 					</div>
+// // 				</div>
+
+// // 				<div className='flex flex-col flex-1'>
+// // 					<div className='flex gap-3 justify-between'>
+// // 						<p className='font-bold text-gray-200'>{conversation.fullName}</p>
+// // 						<span className='text-xl'>{emoji}</span>
+// // 					</div>
+// // 				</div>
+// // 			</div>
+
+// // 			{!lastIdx && <div className='divider my-0 py-0 h-1' />}
+// // 		</>
+// // 	);
+// // };
+// // export default Conversation;
